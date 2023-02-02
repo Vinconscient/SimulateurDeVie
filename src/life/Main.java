d1c325f8055fc3de544c434c0c826bc69738a80e
VlNCONSCIENT <98889021+VlNCONSCIENT@users.noreply.github.com>
2023-02-02 12:00:35 +0100

    un main au TOP moumoute

--- src/life/Main.java
+++ src/life/Main.java
@@ -1,31 +1,24 @@
 package life;
+import java.util.concurrent.TimeUnit;
 
-
-public class Main {
-    public static void main(String args[]) {
-        GameOfLife game = new GameOfLife(5, 5);
-        game.setCellule(2, 3);
-        game.setCellule(3, 3);
-        game.setCellule(3, 2);
-        game.setCellule(1, 2);
-        game.setCellule(3, 1);
-
-        game.print();
-        System.out.println("\n Next gen");
-        System.out.println(game.nbVoisins(1,1) + " " + game.nbVoisins(1,2) + " " + game.nbVoisins(1,3));
-        game.nextGeneration();
-        game.print();
-        System.out.println("\n Next gen");
-        System.out.println(game.nbVoisins(1,1) + " " + game.nbVoisins(1,2) + " " + game.nbVoisins(1,3));
-        game.nextGeneration();
-        game.print();System.out.println("\n Next gen");
-        System.out.println(game.nbVoisins(1,1) + " " + game.nbVoisins(1,2) + " " + game.nbVoisins(1,3));
-        game.nextGeneration();
-        game.print();
-        System.out.println("\n Next gen");
-        System.out.println(game.nbVoisins(1,1) + " " + game.nbVoisins(1,2) + " " + game.nbVoisins(1,3));
-        game.nextGeneration();
-        game.print();
-
+public class Main{
+    public static void main(String args[]){
+        GameOfLife gol = new GameOfLife(92,51);
+        System.out.println(gol);
+        gol.trucDeBase(0);
+        // System.out.println(gol.nbVoisin(3,3));
+        int nbGen = 0;
+        while(true){
+            gol.prochaineGeneration();
+            nbGen = nbGen + 1;
+            // System.out.println("NEW WAVE n°"+nbGen);
+            System.out.println(gol);
+            try {
+            Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }    
+        }
+        
     }
 }
